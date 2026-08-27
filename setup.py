e91918fb2cc9141dd5c3c343d856b8de44bf7dce
Jose Luis SANCHEZ-LOPEZ <joselusl@hotmail.com>
2026-08-27 17:26:46 +0200

    minor updates for compatibility

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
  maintainer_email='joseluis.sanlop@gmail.com',
  description='TODO: Package description',
  license='BSD',
- tests_require=['pytest'],
  entry_points={'console_scripts': [
  		'ars_sim_robot_dynamics_ros_node = ars_sim_robot.ars_sim_robot_dynamics_ros_node:main',
  		'ars_sim_robot_status_ros_node = ars_sim_robot.ars_sim_robot_status_ros_node:main',
